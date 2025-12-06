9bc828a7d65a749016e1b4d8e11aeaf2222f74be
ozturkzeynep374 <ozturkzeynep374@gmail.com>
2025-12-06 17:34:52 +0100

    Update gameover.c

--- src/gameover.c
+++ src/gameover.c
@@ -327,4 +327,42 @@ void drawGameOverScreen() {
     drawLetter(E,       x+24,     y+20,    12,4);
     drawLetter(R,       x+36,     y+20,    12,4);
 }
+void drawTheWinner(){
+    int winnerY = 75; // start y för vinnaren, under "OVER"
+    int winnerX = 50;       // start x, justera efter smak
+    //Om vänster vinner
+    if (leftScore.value > rightScore.value){
+        //LEFT
+        drawLetter(L,winnerX+6,winnerY,14, 4);
+        drawLetter(E,winnerX+12,winnerY,14, 4);
+        drawLetter(F,winnerX+18,winnerY,14, 4);   
+        drawLetter(T,winnerX+24,winnerY,14, 4);
 
+        //WINS
+        drawLetter(W,winnerX+36,winnerY,14, 4);
+        drawLetter(I,winnerX+42,winnerY,14, 4);
+        drawLetter(N,winnerX+48,winnerY,14, 4);
+        drawLetter(S,winnerX+56,winnerY,14, 4);  
+    }
+    else if(rightScore.value > leftScore.value){
+        //RIGHT
+        drawLetter(R,winnerX +6,winnerY,14, 4);
+        drawLetter(I,winnerX+12,winnerY,14, 4);
+        drawLetter(G,winnerX +18,winnerY,14, 4);
+        drawLetter(H,winnerX +24,winnerY,14, 4); 
+        drawLetter(T,winnerX +30,winnerY,14, 4);
+
+        //WINS
+        drawLetter(W,winnerX+36,winnerY,14, 4);
+        drawLetter(I,winnerX+42,winnerY,14, 4);
+        drawLetter(N,winnerX+48,winnerY,14, 4);
+        drawLetter(S,winnerX+56,winnerY,14, 4);   
+    }
+    else{
+        //DRAW
+        drawLetter(D,winnerX+6,winnerY,14, 4);
+        drawLetter(R,winnerX+12,winnerY,14, 4);
+        drawLetter(A,winnerX+18,winnerY,14, 4);   
+        drawLetter(W,winnerX+24,winnerY,14, 4);
+    }
+}
